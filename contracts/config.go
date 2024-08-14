401cfa62552e577b8d5e9e5910e540292a289edc
Darren Kelly <darren.kelly@vechain.org>
2024-08-14 09:54:39 +0100

    refactor: embed json ABIs

--- contracts/config.go
+++ contracts/config.go
@@ -1,11 +1,14 @@
 package contracts
 
 import (
-	"github.com/darrenvechain/b3tr-indexing/contracts/abis"
+	"bytes"
+	"embed"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"strings"
 )
+
+//go:embed abis
+var FS embed.FS
 
 var B3trAddress = common.HexToAddress("0x5ef79995FE8a89e0812330E4378eB2660ceDe699")
 var B3trGovernorAddress = common.HexToAddress("0x1c65C25fABe2fc1bCb82f253fA0C916a322f777C")
@@ -18,19 +21,21 @@ var X2eRewardsAddress = common.HexToAddress("0x6Bee7DDab6c99d5B2Af0554EaEA484CE1
 var XAllocationPoolAddress = common.HexToAddress("0x4191776F05f4bE4848d3f4d587345078B439C7d3")
 var XAllocationVotingAddress = common.HexToAddress("0x89A00Bb0947a30FF95BEeF77a66AEdE3842Fe5B7")
 
-var B3trABI = mustParseABI(abis.B3tr)
-var B3trGovernorABI = mustParseABI(abis.Governor)
-var EmissionsABI = mustParseABI(abis.Emissions)
-var GalaxyMemberABI = mustParseABI(abis.GM)
-var TreasuryABI = mustParseABI(abis.Treasury)
-var Vot3ABI = mustParseABI(abis.Vot3)
-var X2eAppsABI = mustParseABI(abis.X2eApps)
-var X2eRewardsABI = mustParseABI(abis.X2eRewardsPool)
-var XAllocationPoolABI = mustParseABI(abis.AllocationPool)
-var XAllocationVotingABI = mustParseABI(abis.AllocationVoting)
+var B3trABI = mustParseABI("b3tr.json")
+var B3trGovernorABI = mustParseABI("governor.json")
+var EmissionsABI = mustParseABI("emissions.json")
+var GalaxyMemberABI = mustParseABI("galaxy-member.json")
+var TreasuryABI = mustParseABI("treasury.json")
+var Vot3ABI = mustParseABI("vot3.json")
+var X2eAppsABI = mustParseABI("x2e_apps.json")
+var X2eRewardsABI = mustParseABI("x2e_rewards_pool.json")
+var XAllocationPoolABI = mustParseABI("x_allocation_pool.json")
+var XAllocationVotingABI = mustParseABI("x_allocation_voting.json")
 
-func mustParseABI(contractABI string) abi.ABI {
-	contract, err := abi.JSON(strings.NewReader(contractABI))
+func mustParseABI(path string) abi.ABI {
+	content, err := FS.ReadFile("abis/" + path)
+	reader := bytes.NewReader(content)
+	contract, err := abi.JSON(reader)
 	if err != nil {
 		panic(err)
 	}
